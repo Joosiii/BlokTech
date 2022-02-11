ad23334558a28568b27180953da8c5de3138179f
Joosiii <joost.verweijen@hva.nl>
2022-02-11 14:40:17 +0100

    Commit met app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+
+
+app.get('/', (req, res) => {
+
+res.send('Dikke piemel')
+
+})
+
+
+
+app.listen(port, () => {
+
+console.log(`Example app listening on port ${port}`)
+
+})
