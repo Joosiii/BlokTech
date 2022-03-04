d5cbb6b6cbf9bbe8c6977688800b95c246ba580e
Joost Verweijen <joosiiiv@gmail.com>
2022-03-04 02:49:12 +0100

    Nieuwste Versie

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 4000
 
+const profiles = [
+    {
+        id: 1,
+        name: "Hans",
+        age: "27",
+        bio: "Just an adventurous, spontaneous guy looking for an animal-loving match!",
+        interests: ["Travel", "Dogs", "Cooking", "Surfing", "Politics"]
+    }
+];
+
+//Static Files
 app.use(express.static('public'))
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+//Set Engine
+app.set('view engine', 'ejs')
+
+//Main Routes
+app.get('/',  (req, res) => {
+    const title  = "Home";
+    res.render('home', {title});
 })
 
-app.get('/', onhome)
-app.get('/profile', onprofile)
+app.get('/profile',  (req, res) => {
+    const title  = "Profile Page";
+    res.render('profile', {title, id: 1,
+        name: "Hans",
+        age: "27",
+        bio: "Just an adventurous, spontaneous guy looking for an animal-loving match!",
+        interests: ["Travel", "Dogs", "Cooking", "Surfing", "Politics"]});
+})
 
-function onhome(req, res) {
-    res.send("<h1>Welkom op de homepagina</h1>")
-}
+app.get('/edit',  (req, res) => {
+    const title  = "Profile Editor";
+    res.render('edit', {title});
+})
 
-function onprofile(req, res) {
-    res.send("<h1>Welkom op de profielpagina</h1>")
-}
-
+// Error Page
 app.use((req, res, next) => {
     res.status(404).send(
-        "<h1>Error: Page Not Found</h1>")
+        "<h1>Error: Page Not Found</h1>");
+})
+
+//Start Server
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
--- public/index.js
+++
@@ -1,6 +0,0 @@
-const name = "Joost";
-const camelCase = require('camelcase');
-
-console.log(`Hello ${name}`);
-
-console.log(camelCase('joost-verweijen'));
